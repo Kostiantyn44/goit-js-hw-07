5f9e420ddf06e68efcf83dc19828754aebea7cbc
Kostiantyn <doroshkostiantyn@gmail.com>
2023-04-09 10:19:47 +0100

    финиш

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+
 // Change code below this line
 
-console.log(galleryItems);
+function createGalleryCard(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+            <li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+    </li>`;
+    })
+    .join("");
+}
+
+const cardsContainer = document.querySelector("ul.gallery");
+const cardsMarkup = createGalleryCard(galleryItems);
+cardsContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+cardsContainer.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const isGalleryItem =
+    event.target.classList.contains("gallery__image") ||
+    event.target.parentNode.classList.contains("gallery__image");
+  if (!isGalleryItem) {
+    return;
+  }
+
+  const galleryImage = event.target.parentNode;
+  const originalImageSrc = galleryImage.getAttribute("href");
+  const imageAlt = galleryImage.querySelector("img").getAttribute("alt");
+
+  const modal = basicLightbox.create(`
+      <div class="modal">
+        <img src="${originalImageSrc}" alt="${imageAlt}" />
+        <button type="button" class="modal__close">Close</button>
+      </div>
+    `);
+
+  modal.show();
+
+  const modalCloseBtn = modal.element().querySelector(".modal__close");
+  modalCloseBtn.addEventListener("click", modal.close);
+
+  modal.element().addEventListener("click", (event) => {
+    if (event.target === modal.element()) {
+      modal.close();
+    }
+  });
+
+  const modalImage = modal.element().querySelector("img");
+  modalImage.addEventListener("click", modal.close);
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+function createGalleryCard(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+            <li class="gallery__item simple-lightbox">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+    </li>`;
+    })
+    .join("");
+}
+
+const cardsContainer = document.querySelector("ul.gallery");
+const cardsMarkup = createGalleryCard(galleryItems);
+cardsContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+cardsContainer.addEventListener("click", onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const isGalleryItem =
+    event.target.classList.contains("gallery__image") ||
+    event.target.parentNode.classList.contains("gallery__image");
+  if (!isGalleryItem) {
+    return;
+  }
+
+  const gallery = new SimpleLightbox(".gallery a", {
+    elementsSelector: ".gallery__item:not(.simple-lightbox)",
+    docClose: false,
+    history: false,
+    fadeSpeed: 100,
+    disableScroll: false,
+  });
+}
